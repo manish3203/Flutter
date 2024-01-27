c2d9398efbd613bfd9279ece82cdc6fad1e875c2
manish3203 <manishchavan3203@gmail.com>
2024-01-27 18:19:43 +0530

    Assignmenet 3

---
+++ Core2Web/Stateless/Assignment 3/assignment_3/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+import 'Assignment3.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    
+    return MaterialApp(
+
+      home: Assignment3(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
