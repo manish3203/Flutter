71f59be236601df545c8efae1b45e5f51f5bb8d1
manish3203 <manishchavan3203@gmail.com>
2024-03-07 21:39:21 +0530

    Day 03

---
+++ Core2Web/Assignment/DailyFlash/Day03/day03/lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:day03/Screens/assignment1.dart';
+import 'package:day03/Screens/assignment2.dart';
+import 'package:day03/Screens/assignment3.dart';
+import 'package:day03/Screens/assignment4.dart';
+import 'package:day03/Screens/assignment5.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Assignment5()
+    );
+  }
+}
