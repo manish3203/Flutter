2ca3d60b73ed05ae4fcc691dc4c62c09480d3087
manish3203 <manishchavan3203@gmail.com>
2024-03-07 21:38:32 +0530

    Day 02

---
+++ Core2Web/Assignment/DailyFlash/Day02/day02/android/app/src/main/kotlin/com/example/day02/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.day02
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ Core2Web/Assignment/DailyFlash/Day02/day02/lib/main.dart
@@ -0,0 +1,23 @@
+
+import 'package:day02/Screens/assignment1.dart';
+import 'package:day02/Screens/assignment2.dart';
+import 'package:day02/Screens/assignment3.dart';
+import 'package:day02/Screens/assignment4.dart';
+import 'package:day02/Screens/assignment5.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Assignment5()
+    );
+  }
+}
