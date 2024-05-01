90fa18655773457daf6d931c029f85178c512c6a
manish3203 <manishchavan3203@gmail.com>
2024-05-01 11:46:20 +0530

    Assignment 22

---
+++ Core2Web/Statefull/Assignment 22/advance_to_do_ui1/android/app/src/main/kotlin/com/example/advance_to_do_ui1/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.advance_to_do_ui1
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Core2Web/Statefull/Assignment 22/advance_to_do_ui1/lib/Utils/utils.dart
@@ -0,0 +1,17 @@
+
+import 'dart:ui';
+
+import 'package:advance_to_do_ui1/main.dart';
+
+//List of colors
+List colorList = [
+    const Color.fromRGBO(250, 232, 232, 1),
+    const Color.fromRGBO(232, 237, 250, 1),
+    const Color.fromRGBO(250, 249, 232, 1),
+    const Color.fromRGBO(250, 232, 250, 1)
+];
+
+//List of data to see in cards
+
+List<ToDOModel> taskList = [];
+
